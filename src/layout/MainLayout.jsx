4570449fbe02aa75675d4a9be59165a3294b70dd
yaminmusleh <yaminaktham3@gmail.com>
2026-02-12 18:52:11 +0200

    made register (sign up) design in Register.jsx

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Navbar from '../components/navbar/navbar'
-import Footer from '../components/footer/footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navbar from "../components/navbar/navbar";
+import Footer from "../components/footer/footer";
+import { Outlet } from "react-router-dom";
+import { Container } from "@mui/material";
 
 export default function MainLayout() {
   return (
-   <>
-   <Navbar/>
-   <Outlet/>
-   <Footer/>
-   </>
-  )
+    <>
+      <Navbar />
+      <Container>
+        <Outlet />
+      </Container>
+      <Footer />
+    </>
+  );
 }
--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
-
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Button, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
 export default function Register() {
   return (
-    <div>Register</div>
-  )
+    <Box
+      component={"section"}
+      className="register-form"
+      sx={{ paddingY: "70px" }}
+    >
+      <Container
+        maxWidth="md"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          rowGap: 5,
+        }}
+      >
+        <Typography
+          component={"h1"}
+          variant="h3"
+          sx={{
+            fontFamily: "poppins",
+            color: "#503217",
+            fontSize: { xs: "25px", sm: "35px", md: "40px" },
+            fontWeight: 600,
+          }}
+        >
+          Welcome to Medifit — Your Partner in Smarter Health
+        </Typography>
+
+        <Box
+          component={"form"}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            rowGap: "30px",
+            width: { xs: "90%", sm: "450px", md: "600px" },
+          }}
+        >
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+          <TextField id="outlined-basic" label="User Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Full Name" variant="outlined" />
+          <TextField
+            id="outlined-basic"
+            label="Phone number"
+            variant="outlined"
+          />
+          <Button
+            variant="contained"
+            sx={{
+              width: "40%",
+              alignSelf: "center",
+              fontFamily: "poppins",
+              textTransform: "none",
+              marginTop: "20px",
+              backgroundColor: "#503217",
+              borderRadius: "5px",
+              fontSize:"15px"
+            }}
+          >
+            Sign Up!
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
