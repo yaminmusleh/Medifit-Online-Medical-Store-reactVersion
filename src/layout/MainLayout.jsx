86793f72e3491d36e326db5512e84776554e54f2
yaminmusleh <yaminaktham3@gmail.com>
2026-02-13 18:08:04 +0200

    fixed navbar and footer import

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../components/navbar/navbar";
-import Footer from "../components/footer/footer";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 import { Outlet } from "react-router-dom";
 import { Container } from "@mui/material";
 
