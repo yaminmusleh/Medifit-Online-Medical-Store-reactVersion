33df6f14160a25d9aa7a1a59f161e2ecc501db9d
yaminmusleh <yaminaktham3@gmail.com>
2026-02-14 21:10:31 +0200

    added Accept-Language in instance axios js file to solve language problem

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 const instance = axios.create({
   baseURL: "https://knowledgeshop.runasp.net/api",
+  headers: {
+    "Accept-Language": "en",
+  },
 });
 export default instance;
