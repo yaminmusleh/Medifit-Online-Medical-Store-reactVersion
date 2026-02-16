71fe19dc6045ebdc446ee966dae8b6ee40335806
yaminmusleh <yaminaktham3@gmail.com>
2026-02-16 22:52:10 +0200

    finished category cards in the main home

--- src/categories/Categories.jsx
+++ src/categories/Categories.jsx
@@ -4,9 +4,16 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import useCategs from "../hooks/useCategs";
+import image1 from "./shop-by-categ-imgs/frame1.svg";
+import image2 from "./shop-by-categ-imgs/frame2.svg";
+import image3 from "./shop-by-categ-imgs/frame3.svg";
+import image4 from "./shop-by-categ-imgs/frame4.svg";
+import image5 from "./shop-by-categ-imgs/frame5.svg";
+import image6 from "./shop-by-categ-imgs/frame6.svg";
 
 export default function Categories() {
   const number = [32, 20, 30, 35, 25, 35, 30];
+  const images = [image1, image2, image3, image4, image5, image6];
 
   const { data, isLoading, isError, error } = useCategs();
   //data, isLoading, isError, error takes the values from the query that we returned in useCategs
@@ -72,11 +79,11 @@ export default function Categories() {
           <Card
             key={categs.id}
             sx={{
-              flexBasis: {
-                xs: "50%",
-                sm: "30%",
-                md: "20%",
-                lg: "15%",
+              flex: {
+                xs: "1 1 40%",
+                sm: "1 1 30%",
+                md: "1 1 20%",
+                lg: "1 1 15%",
               },
               borderRadius: "20px",
               height: "200px",
@@ -90,11 +97,35 @@ export default function Categories() {
                 height: "100%",
                 display: "flex",
                 flexDirection: "column",
+                textAlign: "center",
               }}
             >
               <CardContent
-                sx={{ flex: 1, display: "flex", flexDirection: "column" }}
+                sx={{
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
               >
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                  }}
+                >
+                  <img
+                    style={{
+                      display: "block",
+                      width: "80%",
+                    }}
+                    src={images[index % images.length]}
+                    alt="image"
+                  />
+                </Box>
                 <Typography
                   sx={{
                     fontSize: "16px",
@@ -108,7 +139,15 @@ export default function Categories() {
                 >
                   {categs.name}
                 </Typography>
-                <Typography sx={{color:'#8F7D6A',fontFamily:'poppins',fontSize:'13.5px'}}>{number[index % number.length]} Items</Typography>
+                <Typography
+                  sx={{
+                    color: "#8F7D6A",
+                    fontFamily: "poppins",
+                    fontSize: "13.5px",
+                  }}
+                >
+                  {number[index % number.length]} Items
+                </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
