e5e136a0109d21954dac007a43e31b69497de4d2
yaminmusleh <yaminaktham3@gmail.com>
2026-02-24 21:07:19 +0200

    added AboutUs.jsx component under the AboutTop in about page

---
+++ src/components/about_us/AboutUs.jsx
@@ -0,0 +1,51 @@
+import { Box, Grid, Typography } from "@mui/material";
+import React from "react";
+
+export default function AboutUs({ MainColor, MainFont }) {
+  return (
+    <Box py={"130px"} display={"flex"} justifyContent={"center"}>
+      <Grid
+        container
+        sx={{ justifyContent: "center", alignItems: "start" }}
+        spacing={8}
+      >
+        <Grid item xs={12} md={4}>
+          <Typography
+            sx={{
+              textAlign: "end",
+              fontSize: {
+                xs: "34px",
+                sm: "40px",
+                md: "55px",
+              },
+              color: MainColor,
+              fontFamily: MainFont,
+            }}
+          >
+            About us
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ width: "70%" }}>
+          <Typography
+            sx={{
+              textAlign: {
+                xs: "center",
+                lg: "start",
+              },
+              fontSize: {
+                xs: "20px",
+                sm: "30px",
+                md: "40px",
+              },
+              color: MainColor,
+              fontFamily: MainFont,
+            }}
+          >
+            Our focus is on nurturing wellness and empowering health for all
+            ages well being for people worldwide.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,12 +4,15 @@ import { Box, Grid, Typography } from "@mui/material";
 import video from "./assets_about/IMG_5209.MP4";
 import poster from "./assets_about/ChatGPT Image Oct 25, 2025, 02_10_06 PM.webp";
 import AboutTop from "../../components/about_top/AboutTop";
+import AboutUs from "../../components/about_us/AboutUs";
 
 export default function About() {
   const color1 = "#503217";
+  const font = 'poppins';
   return (
     <>
     <AboutTop MainColor={color1}/>
+    <AboutUs MainColor={color1} MainFont={font}/>
       <Grid container justifyContent="center" alignItems="center" gap={3}>
         <Typography
           sx={{
