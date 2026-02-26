abd236c97645bfdce4d049199d3acef067e88e52
yaminmusleh <yaminaktham3@gmail.com>
2026-02-26 23:28:18 +0200

    fixed Box and circular progress imports

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Loader() {
   return (
-      <Box
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        <CircularProgress />
-      </Box>
-  )
+    <Box
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <CircularProgress />
+    </Box>
+  );
 }
