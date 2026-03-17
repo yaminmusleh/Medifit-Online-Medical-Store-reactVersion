a797dc2488e278719a77529268f1147b26091a51
yaminmusleh <yaminaktham3@gmail.com>
2026-03-17 15:26:01 +0200

    added contact component to the component page + not ready to use yet (beta version)

---
+++ src/components/contactForm/ContactForm.jsx
@@ -0,0 +1,241 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import phone from "./imgs/phone.svg";
+import user1 from "./imgs/user1.svg";
+import user2 from "./imgs/user2.svg";
+import user3 from "./imgs/user3.svg";
+import user4 from "./imgs/user4.svg";
+import user5 from "./imgs/user5.svg";
+import { toast } from "react-toastify";
+
+export default function ContactForm({ MainColor, MainFont }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    schedule: "",
+    message: "",
+  });
+
+  const isFormIncomplete = Object.values(formData).some(
+    (value) => value.trim() === "",
+  );
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const storedMessages =
+      JSON.parse(localStorage.getItem("contactMessages")) || [];
+    storedMessages.push(formData);
+    localStorage.setItem("contactMessages", JSON.stringify(storedMessages));
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      schedule: "",
+      message: "",
+    });
+    toast.success("Form got submitted to our team!");
+  };
+
+
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "70px",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        className="title"
+        sx={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+        }}
+      >
+        <Typography
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: {
+              xs: "30px",
+              sm: "40px",
+              md: "50px",
+            },
+            color: MainColor,
+            fontFamily: MainFont,
+            fontWeight: 600,
+          }}
+        >
+          Contact us <img src={phone} alt="phone"></img>
+        </Typography>
+        <Typography
+          component={"span"}
+          sx={{ color: "#8F7D6A", fontFamily: "sans-serif" }}
+        >
+          Have questions or need support? Get in touch with our team – we're
+          here to help!
+        </Typography>
+      </Box>
+      <Box
+        component={"form"}
+        onSubmit={handleSubmit}
+        sx={{ display: "flex", flexDirection: "column", gap: "50px" }}
+      >
+        <Box className="upper" sx={{ display: "flex", gap: "20px" }}>
+          <Box sx={{ width: "418px" }}>
+            <Typography
+              sx={{
+                fontFamily: "sans-serif",
+                fontSize: { xs: 13, md: 15 },
+                color: MainColor,
+              }}
+            >
+              Full name
+            </Typography>
+            <TextField
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "14px",
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+
+          <Box sx={{ width: "418px" }}>
+            <Typography
+              sx={{
+                fontFamily: "sans-serif",
+                fontSize: { xs: 13, md: 15 },
+                color: MainColor,
+              }}
+            >
+              Email address
+            </Typography>
+            <TextField
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "14px",
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+        </Box>
+        <Box className="lower" sx={{ display: "flex", gap: "20px" }}>
+          <Box sx={{ width: "418px" }}>
+            <Typography
+              sx={{
+                fontFamily: "sans-serif",
+                fontSize: { xs: 13, md: 15 },
+                color: MainColor,
+              }}
+            >
+              Phone number
+            </Typography>
+            <TextField
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "14px",
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+
+          <Box sx={{ width: "418px" }}>
+            <Typography
+              sx={{
+                fontFamily: "sans-serif",
+                fontSize: { xs: 13, md: 15 },
+                color: MainColor,
+              }}
+            >
+              Schedule to receive call
+            </Typography>
+            <TextField
+              name="schedule"
+              value={formData.schedule}
+              onChange={handleChange}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "14px",
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+        </Box>
+        <Box className="message">
+          <Typography
+            sx={{
+              fontFamily: "sans-serif",
+              fontSize: { xs: 13, md: 15 },
+              color: MainColor,
+            }}
+          >
+            Message
+          </Typography>
+          <TextField
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            multiline
+            rows={7}
+            fullWidth
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "14px",
+                alignItems: "flex-start",
+              },
+            }}
+          />
+        </Box>
+        <Box>
+          <Button
+            disabled={isFormIncomplete}
+            type="submit"
+            
+            sx={{
+              backgroundColor: MainColor,
+              fontFamily: MainFont,
+              textTransform: "none",
+            }}
+            variant="contained"
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Invitation from '../../components/about_invitation/Invitation'
 import FrequentQuestions from '../../components/frequentQuestions/FrequentQuestions'
+import ContactForm from '../../components/contactForm/ContactForm'
 
 export default function Contact({MainColor,MainFont}) {
   return (
     <>
+    <ContactForm MainColor={MainColor} MainFont = {MainFont}/>
     <FrequentQuestions MainColor={MainColor} MainFont = {MainFont}/>
     <Invitation/>
     </>
